181114271304cf7d4819237c2c27c1403d57feaf
mohammei <mohammedismal@hotmail.com>
2015-03-12 08:59:39 +0100

    her gjørt par endringer på de fleste av dem og lagt til html filer

--- djangoprojects/mysite/mysite/settings.py
+++ djangoprojects/mysite/mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# template
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -54,7 +57,6 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # TEMP
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
--- djangoprojects/mysite/polls/admin.py
+++ djangoprojects/mysite/polls/admin.py
@@ -14,9 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
 
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
-    search_fields = ['question_text']
-
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
 
 
